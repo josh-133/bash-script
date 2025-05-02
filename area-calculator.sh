74b7fa2e1895f13c0717238004fae2bd04fc0369
hello world <joshuaburge507@gmail.com>
2025-05-02 12:10:06 +0930

    got a simple area calculator that can calculate the area of square triangle rectangle and circle

--- area-calculator.sh
+++ area-calculator.sh
@@ -13,5 +13,53 @@ type_text() {
     echo
 }
 
-type_text "What shape would you like to calculate the area of?"
+calculate_area() {
+    case $1 in
+        rectangle)
+            area=$(echo "$2 * $3" | bc)
+            ;;
+        square)
+            area=$(echo "$2 * $2" | bc)
+            ;;
+        triangle)
+            area=$(echo "0.5 * $2 * $3" | bc)
+            ;;
+        circle)
+            area=$(echo "3.14159 * $2 * $2" | bc)
+            ;;
+        *)
+            type_text "Sorry I don't know how to calculate the area of $1." 0.08
+            return
+            ;;
+    esac
+
+    type_text "The area of the $1 is $area units." 0.08
+}
+
+type_text "What shape would you like to calculate the area of?" 0.08
 read shape
+
+shape=$(echo "$shape" | tr '[:upper]' '[:lower]')
+
+case $shape in
+    rectangle|triangle)
+        type_text "Enter the base/length" 0.08
+        read length
+        type_text "Enter the height/width" 0.08
+        read width
+        calculate_area "$shape" "$length" "$width"
+        ;;
+    square)
+        type_text "Enter the length of one side" 0.08
+        read length
+        calculate_area "$shape" "$length"
+        ;;
+    circle)
+        type_text "Enter the radius" 0.08
+        read radius
+        calculate_area "$shape" "$radius"
+        ;;
+    *)
+        type_text "Unknown shape $shape" 0.08
+        ;;
+esac
